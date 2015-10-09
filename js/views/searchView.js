77bf21b845a22e5a5d932a142488242c8ebfb9b7
Alexey Drozdov <alexey.drozdov@gmail.com>
2015-10-09 20:56:54 +0300

    Create logic for adding new item after search performed

--- js/views/searchView.js
+++ js/views/searchView.js
@@ -18,6 +18,37 @@ var app = app || {};
 
 		initialize: function () {
 			this.$search = $('#new-search');
+			this.$searchMain = this.$('#searchMain');
+			this.$searchResults = this.$('#searchResults');
+			this.$list = $('#search-list');
+
+			this.listenTo(app.searchItems, 'add', this.addOne);
+			this.listenTo(app.searchItems, 'reset', this.addAll);
+			this.listenTo(app.searchItems, 'all', this.render);
+
+		},
+
+		// Re-rendering the App just means refreshing the statistics -- the rest
+		// of the app doesn't change.
+		render: function () {
+			if (app.searchItems.length) {
+				this.$searchResults.show();
+			} else {
+				this.$searchResults.hide();
+			}
+		},
+
+		// Add a single todo item to the list by creating a view for it, and
+		// appending its element to the `<ul>`.
+		addOne: function (item) {
+			var view = new app.SearchItemView({ model: item });
+			this.$list.append(view.render().el);
+		},
+
+		// Add all items in the **Todos** collection at once.
+		addAll: function () {
+			this.$list.html('');
+			app.searchItems.each(this.addOne, this);
 		},
 
 		// Generate the attributes for a new Search Item.
